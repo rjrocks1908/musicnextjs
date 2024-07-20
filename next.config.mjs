cc6a1ca79cf279ef5811b8da23be14a54e01baac
Rahul Jain <rj.rahul1908@gmail.com>
2024-07-20 20:39:12 +0530

    Init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    domains: ["images.unsplash.com", "res.cloudinary.com"],
+  },
+};
 
 export default nextConfig;
---
+++ src/app/courses/[slug]/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import musicData from "@/data/music_courses.json";
+import Image from "next/image";
+import { Button } from "@/components/ui/moving-border";
+
+function page({ params }: { params: { slug: string } }) {
+  const course = musicData.courses.find(
+    (course) => course.slug === params.slug
+  );
+
+  if (!course) {
+    return (
+      <div className="min-h-screen w-full flex items-center justify-center text-2xl text-white">
+        404 | Course Not Found
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen bg-black py-12 pt-36 px-10">
+      <h1 className="text-lg md:text-7xl font-sans font-bold mb-8 text-white">
+        {course.title}
+      </h1>
+      <Image
+        src={course.image}
+        height="1000"
+        width="1000"
+        className="h-60 w-full max-w-lg object-cover rounded-xl"
+        alt={course.title}
+      />
+      <p className="py-2 font-bold">{course.description}</p>
+      <p>- {course.instructor}</p>
+      <Button>{course.price}</Button>
+    </div>
+  );
+}
+
+export default page;
